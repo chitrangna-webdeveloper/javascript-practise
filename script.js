a94957289b3219f64dfe4c2c53a928d92739c835
chitrangna <chitrangna.dev@gmail.com>
2026-05-31 10:56:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+//Answer-1
+function greet(name){
+    console.log(name);
+}
+greet("hello"+" "+"name");
+
+//Answer-2
+function add(a,b){
+    console.log(a+b);
+}
+add(4,3);
+
+//Answer-3
+function square(a){
+    console.log(a*a);
+}
+square(5);
+
+//Answer-4
+function multiply(a,b){
+    console.log(a*b);
+}
+multiply(2,3);
+
+//Answer-5
+function maxoftwo(a,b){
+
+    if(a>b){
+        console.log(a);
+    }
+    else{
+        console.log(b);
+    }
+}
+maxoftwo(7,4);
+
+//Answer-6
+function kelvin(c){
+    console.log(k=273+c);
+}
+kelvin(4);
+
+//Answer-7
+function even(a){
+
+    if(a%2===0){
+        console.log("even");
+    }
+    else{
+        console.log("odd");
+    }
+}
+even(4);
+
+//Answer-8
+function areaofcircle(r){
+    console.log(A=22/7*r*r);
+}
+areaofcircle(4);
+
+//Answer-9
+function factorial(n){
+    console.log();
+}
+factorial(5);
