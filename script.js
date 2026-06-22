d108ee92c1fda76ff8b67f2b1d705406d1ce5c6d
chitrangna <chitrangna.dev@gmail.com>
2026-06-22 19:31:41 +0530

    Refactor script.js to include input handling

--- script.js
+++ script.js
@@ -200,5 +200,45 @@
     console.log(i+i);
 
  }
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+
+let addbtn = document.getElementById("addbtn");
+let subbtn = document.getElementById("subbtn");
+let multiplybtn = document.getElementById("multiplybtn");
+let dividebtn = document.getElementById("dividebtn");
+
+function checkInputs() {
+    if (num1.value === "" || num2.value === "") {
+        alert("Please fill both input boxes!");
+        return false;
+    }
+    return true;
+}
+
+addbtn.addEventListener("click" , function(){
+    console.log(Number(num1.value)+Number(num2.value));
+    if (!checkInputs()) return;
+
+
+});
+subbtn.addEventListener("click" , function(){
+    console.log(Number(num1.value)-Number(num2.value));
+    if (!checkInputs()) return;
+
+
+});
+multiplybtn.addEventListener("click" , function(){
+    console.log(Number(num1.value)*Number(num2.value));
+    if (!checkInputs()) return;
+
+
+});
+dividebtn.addEventListener("click" , function(){
+    console.log(Number(num1.value)/Number(num2.value));
+    if (!checkInputs()) return;
+
+
+});
 
  
